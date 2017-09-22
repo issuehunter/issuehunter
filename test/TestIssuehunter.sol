c9ecbe5fc23d8a25966cf109cd2a51fe250dee54
Giovanni Cappellotto <potomak84@gmail.com>
2017-09-22 15:43:31 -0400

    Add createCampaignWithVerifier function
    The new function makes it possible to create a new campaign that
    has a custom address as a verifier.
    
    This is the easier way to support different oracles. Instead of
    creating a new oracle interface, let's keep things simple by
    allowing a custom address to verify campaign's patches.
    
    See also https://github.com/issuehunter/issuehunter/issues/5.

--- test/TestIssuehunter.sol
+++ test/TestIssuehunter.sol
@@ -10,13 +10,13 @@ contract TestIssuehunter {
     function testInitialIssueManagerUsingDeployedContract() {
         Issuehunter issuehunter = Issuehunter(DeployedAddresses.Issuehunter());
 
-        Assert.equal(issuehunter.patchVerifier(), tx.origin, "Contract creator should be the patch verifier");
+        Assert.equal(issuehunter.defaultPatchVerifier(), tx.origin, "Contract creator should be the default patch verifier");
     }
 
     function testInitialIssueManagerWithNewIssuehunter() {
         Issuehunter issuehunter = new Issuehunter();
 
-        Assert.equal(issuehunter.patchVerifier(), tx.origin, "Contract creator should be the patch verifier");
+        Assert.equal(issuehunter.defaultPatchVerifier(), tx.origin, "Contract creator should be the default patch verifier");
     }
 
 }
